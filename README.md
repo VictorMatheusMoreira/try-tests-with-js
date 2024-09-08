b29c01074bf3cea7f0b76fca967ddb8ed964db25
VictorMatheusMoreira <vi.matheusm@gmail.com>
2024-09-08 19:12:16 -0300

    first commit
    just not to lose it

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # try-tests-with-js
 I don't know anything about tests !!!  I'll try to learn it or at least some basic concepts.
+
+npm init -y
+
+npx eslint --init
+
+
+
---
+++ src/entities/appointment.ts
@@ -0,0 +1,11 @@
+export class Appointment {
+    private customer: string;
+    private startsAt: Date;
+    private endAt: Date;
+
+    get customer(){
+        return this.customer
+    }
+
+    set 
+}
